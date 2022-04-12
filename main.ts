529182aa4f2c442d8fba03a3b296df392121a13d
une-personne <103508249+une-personne@users.noreply.github.com>
2022-04-12 14:37:46 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Skull)
-})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(25)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(450)
 })
